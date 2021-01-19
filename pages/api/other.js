609d509e8a8847908dddad9d88643f214518569a
eitan schreiber <eitanschreiber@eitans-MacBook-Air.local>
2021-01-18 19:48:20 -0500

    compact 2

--- pages/api/other.js
+++ pages/api/other.js
@@ -1,19 +1,8 @@
 import nodemailer from "nodemailer"
 const emailPass = "qbmD6Zs8Qv76b96"
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "eitanschreiber97@gmail.com",
-        pass: emailPass
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  })
+const transporter=nodemailer.createTransport({host:"smtp.gmail.com",port:465,secure:true,auth:{user:"eitanschreiber97@gmail.com",pass:emailPass},tls:{rejectUnauthorized:false}})
 export default async (req, res) => {
-    const { senderMail, name, origin, affair, message } = req.body
+    const {senderMail,name,origin,affair,message}=req.body
     if (name === "" || origin === "" || affair === "" || message === "") {
         res.status(403).send("")
         return
@@ -22,15 +11,7 @@ export default async (req, res) => {
     res.send(mailerRes)
 }
 const mailer = ({ senderMail, name, origin, affair, message }) => {
-    const emailBody = {
-        senderMail,
-        to: `${senderMail}`,
-        subject: `Message`,
-        text: `name: ${name}
-        affair: ${affair}
-        message: ${message}`,
-        replyTo: origin
-    }
+    const emailBody={senderMail,to:`${senderMail}`,subject:`Message`,text:`name: ${name}affair: ${affair}message: ${message}`,replyTo:origin}
     return new Promise((resolve, reject) => {
         transporter.sendMail(emailBody, (error, info) =>
             error ? reject(error) : resolve(info)
