766334af927639a84f4663d3e8405536186cb491
eitan schreiber <eitanschreiber@eitans-MacBook-Air.local>
2021-01-19 10:46:56 -0500

    remi images 2

--- pages/services.js
+++ pages/services.js
@@ -15,7 +15,7 @@ import { GiCoffeePot } from 'react-icons/gi'
 import { GiMirrorMirror } from 'react-icons/gi'
 import { ImAirplane } from 'react-icons/im'
 import { BsFillPeopleFill } from 'react-icons/bs'
-const Wrapper = styled.div`font-family:Playfair Display;main>section{background-color:white;display:flex;flex-direction:column;align-items:center;padding:0px 20px;}main>section>div:first-of-type{display:grid;grid-template-columns:repeat(5, 100px);grid-gap:30px 25px;}main>section>div:first-of-type>div{display:flex;flex-direction:column;}main>section>div:last-of-type{display:flex;flex-direction:column;margin-bottom:90px;}main>section>div:last-of-type>div{display:flex;flex-direction:row;}main>section>div:last-of-type>div>p{margin-right:50px;}h1{color:rgb(97,26,30);}`
+const Wrapper = styled.div`font-family:Playfair Display;main>section{background-color:white;display:flex;flex-direction:column;align-items:center;padding:0px 20px;}main>section>div:first-of-type{display:grid;grid-template-columns:repeat(5, 100px);grid-gap:30px 25px;}main>section>div:first-of-type>div{display:flex;flex-direction:column;text-align:center;}main>section>div:last-of-type{display:flex;flex-direction:column;margin-bottom:90px;}main>section>div:last-of-type>div{display:flex;flex-direction:row;}main>section>div:last-of-type>div>p{margin-right:50px;}h1{color:rgb(97,26,30);}`
 class ServicesPage extends Component {
   constructor(props) {
     super(props);
